14a2c1c3c7bfe45dbf37f9a6780785bb1ef93a4b
Edmund Rwegasira <edmundrwegasira@outlook.com>
2025-05-08 15:45:58 +0300

    Updated static files config and settings

--- solita_salon/settings.py
+++ solita_salon/settings.py
@@ -133,9 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # collectstatic will put files here
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
