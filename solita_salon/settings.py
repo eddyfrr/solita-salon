979677d5412ebf69fa599d2ca6c4ebe4a1b2504a
Edmund Rwegasira <edmundrwegasira@outlook.com>
2025-05-23 13:19:27 +0300

    Prepare for Railway deployment with Elastic Email

--- solita_salon/settings.py
+++ solita_salon/settings.py
@@ -91,12 +91,11 @@ WSGI_APPLICATION = 'solita_salon.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL', default='postgresql://solita_admin:password@localhost:5432/solita_db'),
-        conn_max_age=600
-    )
-}
-    
+      'default': dj_database_url.config(
+          default=config('DATABASE_URL', default='postgresql://solita_admin:password@localhost:5432/solita_db'),
+          conn_max_age=600
+      )
+  }
 
 
 
