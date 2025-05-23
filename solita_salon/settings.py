82c9c1ac7182dcae08b765f57514c4bb9c352dbb
Edmund Rwegasira <edmundrwegasira@outlook.com>
2025-05-23 18:03:55 +0300

    Add favicon and update urls.py

--- solita_salon/settings.py
+++ solita_salon/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # collectstatic will put files here
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Render-specific settings
--- solita_salon/urls.py
+++ solita_salon/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico',permanent=True)),
     path('admin/', admin.site.urls),
     path('', include('booking.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
